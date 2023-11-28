0a43e122b97d5e6eab361f2a01aa919898f918af
Markus Malz <markus.malz@outlook.com>
2023-11-28 12:00:11 +0100

    Salty

--- Program.cs
+++ Program.cs
@@ -8,30 +8,35 @@ class Program
     static void Main()
     {
         // Your string to encrypt
-        string originalString = "Hello, World!";
+        Console.WriteLine("String for Encryption/Decryption: ");
+        string originalString = Console.ReadLine();
 
+        Console.WriteLine("Password: ");
         // Password to derive the key from
-        string password = "YourPassword";
+        string password = Console.ReadLine();
+
+        Console.WriteLine("Salt (default: SaltForEncryption): ");
+        // Password to derive the key from
+        string salt = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(salt)) salt = "SaltForEncryption";
 
         // Encrypt the string
-        string encryptedString = EncryptString(originalString, password);
+        string encryptedString = EncryptString(originalString, password, salt);
+        // Decrypt the string
+        string decryptedString = DecryptString(originalString, password, salt);
 
         Console.WriteLine("Original String: " + originalString);
         Console.WriteLine("Encrypted String: " + encryptedString);
-
-        // Decrypt the string
-        string decryptedString = DecryptString(encryptedString, password);
-
         Console.WriteLine("Decrypted String: " + decryptedString);
 
         Console.ReadLine();
     }
 
-    static string EncryptString(string plainText, string password)
+    static string EncryptString(string plainText, string password, string salt)
     {
         using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
         {
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes("SaltForEncryption"));
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(salt));
 
             aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
             aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
@@ -53,11 +58,11 @@ class Program
         }
     }
 
-    static string DecryptString(string cipherText, string password)
+    static string DecryptString(string cipherText, string password, string salt)
     {
         using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
         {
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes("SaltForEncryption"));
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(salt));
 
             aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
             aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
