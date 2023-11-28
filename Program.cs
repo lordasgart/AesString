8c1e11dc18459a43faea3f168fb3feb1289e2613
Markus Malz <markus.malz@outlook.com>
2023-11-28 11:53:18 +0100

    Basics how to encrypt and decrypt a string with C#

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // Your string to encrypt
+        string originalString = "Hello, World!";
+
+        // Password to derive the key from
+        string password = "YourPassword";
+
+        // Encrypt the string
+        string encryptedString = EncryptString(originalString, password);
+
+        Console.WriteLine("Original String: " + originalString);
+        Console.WriteLine("Encrypted String: " + encryptedString);
+
+        // Decrypt the string
+        string decryptedString = DecryptString(encryptedString, password);
+
+        Console.WriteLine("Decrypted String: " + decryptedString);
+
+        Console.ReadLine();
+    }
+
+    static string EncryptString(string plainText, string password)
+    {
+        using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+        {
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes("SaltForEncryption"));
+
+            aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+            aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                }
+
+                return Convert.ToBase64String(msEncrypt.ToArray());
+            }
+        }
+    }
+
+    static string DecryptString(string cipherText, string password)
+    {
+        using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+        {
+            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes("SaltForEncryption"));
+
+            aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+            aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
